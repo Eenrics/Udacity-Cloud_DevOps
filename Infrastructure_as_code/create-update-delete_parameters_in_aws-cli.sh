e96694c057a7fe1ecfb23314224bedf409e13a96
Ebenezer Exetie <104176127+Eenrics@users.noreply.github.com>
2022-12-17 11:08:48 +0300

    Create create-update-delete_parameters_in_aws-cli.sh

---
+++ Infrastructure_as_code/create-update-delete_parameters_in_aws-cli.sh
@@ -0,0 +1,17 @@
+# To list all parameters we have
+aws ssm describe-parameters
+
+# To see the detail of a parameter of our choice with its values
+aws ssm get-parameters --names "<parameter-name>"
+
+# To see the parameter history
+aws ssm get-parameter-history --name "<parameter-name>"
+
+# To create new parameter
+aws ssm put-parameter --name "<parameter-name>" --value "<parameter-value>" --type String --tags "Key=<parameter-tagKey>,Value=<parameter-tagValue>"
+
+# To change an existing parameter value
+aws ssm put-parameter --name "<parameter-name>" --value "<parameter-value>" --type String --overwrite
+
+# To delete a parameter
+aws ssm delete-parameter --name "<parameter-name>"
