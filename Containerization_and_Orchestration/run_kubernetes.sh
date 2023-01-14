b64cee1f429df6395dea11880428c79cef036423
Ebenezer Exetie <104176127+Eenrics@users.noreply.github.com>
2023-01-14 23:10:16 +0300

    Create run_kubernetes.sh

---
+++ Containerization_and_Orchestration/run_kubernetes.sh
@@ -0,0 +1,14 @@
+# To start claster
+minikube start
+
+# To get information about clusters
+kubectl cluster-info
+
+# To get nodes
+kubectl get nodes
+
+# To list of deployed apps running on Kubernetes
+kubectl get deployments
+
+# If you had one Kubernetes app "test-app" running, and to scale the app up to 6 application instances
+kubectl scale deployments/test-app --replicas=6
