c5337461ce1458052060615aaf205bd30731c89f
Ebenezer Exetie <104176127+Eenrics@users.noreply.github.com>
2023-01-15 13:14:55 +0300

    Update run_kubernetes.sh

--- Containerization_and_Orchestration/run_kubernetes.sh
+++ Containerization_and_Orchestration/run_kubernetes.sh
@@ -15,3 +15,37 @@ kubectl scale deployments/test-app --replicas=6
 
 # Getting the logs from a running pod
 kubectl logs <pod-name>
+
+# Running kubectl
+
+# Your dockerUserName/ContainerName
+dockerpath="noahgift/flasksklearn"
+
+# Run in Docker Hub container with kubernetes
+kubectl run flaskskearlndemo\
+    --generator=run-pod/v1\
+    --image=$dockerpath\
+    --port=80 --labels app=flaskskearlndemo
+
+# List kubernetes pods
+kubectl get pods
+
+# Forward the container port to host
+kubectl port-forward flaskskearlndemo 8000:80
+
+# Debugging
+
+# Check if the pod is running 
+kubectl get pods
+
+# Get more information about that pod. Find the Events header. This should give you a Reason and Message related to the failure.
+kubectl describe pod <POD NAME>
+
+# What is the pod is working & there is likely an issue with the specific node that needs to be debugged.
+
+# Get nodes and check the Ready Status of the node
+kubectl get nodes
+
+# Get more information about that node.
+kubectl describe node <NODE NAME>
+
